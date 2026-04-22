5cfd979c0c1344ced8e1e6c58879787ead176424
nrana <rananitish37@gmail.com>
2026-04-22 23:58:39 +0530

    learnibg url

--- BuildingServer/mangeLog.js
+++ BuildingServer/mangeLog.js
@@ -2,12 +2,12 @@ const http = require("http")
 const fs = require("fs")
 
 const myServer = http.createServer((req, res) =>{
-    const log = `${Date.now()}: New requirest received\n`;
+    const log = `${Date.now()}: ${req.url} New requirest received\n`;
     fs.appendFile("log.txt",log, (err, result)=>{
         res.end("Hello we are server")
     })
 })
 
 myServer.listen(8004, ()=>{
-    console.log("Server started")
+    console.log("Server started!")
 })
